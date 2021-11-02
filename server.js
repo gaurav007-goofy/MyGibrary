c47c1fcd6edc4d08573cc6a3c9dc173cf093c81e
spikee <thegauravsaini007@gmail.com>
2021-11-02 12:04:05 +0530

    initial Author routes

--- server.js
+++ server.js
@@ -6,21 +6,31 @@ if(process.env.NODE_ENV !== 'production')
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index');
+const bodyParser = require('body-parser');
 
+// routes
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+// setting view engine and layouts
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
 
+// Connnecting to Database
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error',error=>console.error(error))
 db.once('open',()=>console.log('Connected to database'))
 
+
 app.use('/',indexRouter);
+app.use('/authors', authorRouter);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>console.log(`server is listening on port ${PORT}`));

c3b4c14f619e01df80ff28985635684230877ac5
spikee <thegauravsaini007@gmail.com>
2021-11-03 14:32:14 +0530

    books create/new

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-if(process.env.NODE_ENV !== 'production')
-{
-    require('dotenv').config()
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 // routes
-const indexRouter = require('./routes/index');
-const authorRouter = require('./routes/authors');
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 // setting view engine and layouts
-app.set('view engine', 'ejs')
-app.set('views',__dirname + '/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 // Connnecting to Database
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
-const db = mongoose.connection
-db.on('error',error=>console.error(error))
-db.once('open',()=>console.log('Connected to database'))
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
-
-app.use('/',indexRouter);
-app.use('/authors', authorRouter);
-
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>console.log(`server is listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
